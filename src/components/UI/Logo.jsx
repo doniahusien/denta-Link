58dea2602087d5e78ed95e87852afe9f9a0cd4ea
doniahusien <doniahusien@gmail.com>
2025-02-07 03:00:30 +0200

    feat: create logo image component

--- src/components/UI/Logo.jsx
+++ src/components/UI/Logo.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 import Image from 'next/image'
 import logo from '../../../public/logo.svg'
 const Logo = () => {
-  return (
-      <div>
-          <Image src={logo} alt="logo" width={250} height={200}/>
-    </div>
+    return (
+        <div>
+            <Image src={logo} alt="logo" width={250} height={200} />
+        </div>
 
-  )
+    )
 }
 
 export default Logo
