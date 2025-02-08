cb497dbfdd27644c86af08a153ff16df2404a87a
doniahusien <doniahusien@gmail.com>
2025-02-09 00:44:03 +0200

    fix login and signup

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
-
+import LeftImg from '@/components/UI/LeftImg';
+import imgsrc from "../../../../public/images/login.svg";
+import LogInForm from '@/components/auth/LogInForm';
+import GuestRoute from '@/components/GuestRoute';
 const loginPage = () => {
   return (
-    <div>loginPage</div>
+    <GuestRoute>
+      <div className="flex flex-col md:flex-row min-h-screen w-3/4 mx-auto">
+        <LeftImg imgSrc={imgsrc} />
+        <LogInForm />
+      </div>
+    </GuestRoute>
   )
-}
+};
 
-export default loginPage
+export default loginPage;
--- src/app/(auth)/signup/page.jsx
+++ src/app/(auth)/signup/page.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
-
+import LeftImg from '@/components/UI/LeftImg';
+import imgsrc from "../../../../public/images/signup.svg";
+import SignUpForm from '@/components/auth/SignUpForm';
+import GuestRoute from '@/components/GuestRoute';
 const signupPage = () => {
   return (
-    <div>signupPage</div>
+    <GuestRoute>
+      <div className="flex flex-col md:flex-row min-h-screen px-20">
+        <LeftImg imgSrc={imgsrc} />
+        <SignUpForm />
+      </div>
+    </GuestRoute>
   )
-}
+};
 
-export default signupPage
+export default signupPage;
