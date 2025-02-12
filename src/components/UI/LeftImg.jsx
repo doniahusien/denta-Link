8a5752a04289f5dd2b486c0043528564daf7c5d8
doniahusien <doniahusien@gmail.com>
2025-02-12 23:09:46 +0200

    feat(ui): enhance LeftImg and FormWrapper components

--- src/components/UI/LeftImg.jsx
+++ src/components/UI/LeftImg.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import Image from 'next/image'
-const LeftImg = ({imgSrc}) => {
+const LeftImg = ({imgSrc,header}) => {
     return (
-
-        <div className="hidden md:flex flex-1 justify-center items-center ">
+        <div className="hidden md:flex flex-col gap-5 flex-1 justify-center items-center ">
+            {header && <h1 className='text-2xl text-center w-1/2'>{header}</h1>}
             <Image
                 src={imgSrc}
                 alt='signup'
--- src/components/auth/FormWrapper.jsx
+++ src/components/auth/FormWrapper.jsx
@@ -3,9 +3,9 @@
 export default function FormWrapper({ title, children, discription }) {
     return (
         
-        <div className="max-w-sm mx-auto mt-20 bg-white p-6 rounded-xl shadow-xl px-10 py-20">
+        <div className="max-w-sm mx-auto mt-40 bg-white p-6 rounded-xl shadow-xl px-10 py-20">
             <h2 className="text-2xl font-bold mb-8 text-center">{title}</h2>
-            <p  className="text-lg text-gray-400  mb-10 text-center">{discription}</p>
+            <p  className="text-lg text-gray-400 mb-10 text-center">{discription}</p>
             {children}
         </div>
     );
