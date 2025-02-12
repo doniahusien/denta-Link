5b4a8dd4eff7545057ec8fbbce628c140ea246b1
doniahusien <doniahusien@gmail.com>
2025-02-12 23:03:47 +0200

    feat: enhance exchange functionality in market section

--- src/app/market/exchange/page.jsx
+++ src/app/market/exchange/page.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
-
+import ExchangeToolList from '@/components/market/ExchangeToolList'
+import HeaderSection from '@/components/UI/HeaderSection'
+import SearchInput from '@/components/UI/SearchInput'
 const exchangePage = () => {
   return (
-    <div>exchangePage</div>
+    <>
+    <div className="relative pb-5">
+      <HeaderSection imgURL="/images/market/header.png" textDir="center" title="you Know and Trust" subtitle="All the Brands" />
+      <SearchInput href="/market/exchange/add-tool" />
+
+    </div>
+    <div className='pt-10'>
+      <ExchangeToolList />
+    </div>
+
+  </>
   )
 }
 
