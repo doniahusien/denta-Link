25d9e927c7e6b6879e73de6f0348bd91d6828915
doniahusien <doniahusien@gmail.com>
2025-02-14 18:53:54 +0200

    fix: update patient page with new features

--- src/app/patient/page.jsx
+++ src/app/patient/page.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
-
+import HeaderSection from '@/components/UI/HeaderSection'
+import SearchInput from '@/components/UI/SearchInput'
+import PatientList from '@/components/patient/PatientList'
+import ProtectedRoute from '@/components/ProtectedRoute'
 const patientPage = () => {
   return (
-    <div>patient</div>
+    <>
+      <ProtectedRoute>
+        <div className="relative pb-5">
+          <HeaderSection imgURL="/images/patient/patientHeader.svg" textDir="left" title="patient " subtitle="Let’s Find Appropriate" />
+          <SearchInput href="/patient/add-patient" title="Add Patient" placeholder="search patient" />
+
+        </div>
+        <div className='pt-10'>
+          <PatientList />
+        </div>
+      </ProtectedRoute>
+    </>
   )
 }
 
