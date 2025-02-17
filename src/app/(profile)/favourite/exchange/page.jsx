0e20bf7940e5b6d56b41ea0938e1ed65070cb165
doniahusien <doniahusien@gmail.com>
2025-02-18 00:53:12 +0200

    Add new profile pages for editing, favorites, and exchange

---
+++ src/app/(profile)/favourite/exchange/page.jsx
@@ -0,0 +1,59 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import ContentBox from "@/components/UI/profile/ContentBox";
+import ProtectedRoute from "@/components/ProtectedRoute";
+import ExchangeCard from "@/components/market/ExchangeCard";
+
+export default function FavPage() {
+    const [exchanges, setExchanges] = useState([]);
+
+    useEffect(() => {
+        const data = [
+            {
+                publisher: "Dr Ahmed",
+                name: "Cannies",
+                imageSrc: "/images/Profile/mirror.svg",
+                exchangeWith: "Second molar",
+                notes: "The tooth has a cavity.",
+                contact: "015080568",
+                date: "21/1/2025",
+                isFavorite: true,
+            },
+            {
+                publisher: "Dr Sara",
+                name: "Incisor",
+                imageSrc: "/images/Profile/tooth.svg",
+                exchangeWith: "First molar",
+                notes: "This tooth needs a root canal.",
+                contact: "015080569",
+                date: "22/1/2025",
+                isFavorite: true,
+            },
+        ];
+
+        setExchanges(data); 
+    }, []);
+
+    return (
+        <ProtectedRoute>
+            <ContentBox title="Favorite Exchange">
+                <div className="flex flex-col pt-5 gap-5">
+                    {exchanges.map((exchange, index) => (
+                        <ExchangeCard
+                            key={index}
+                            publisher={exchange.publisher}
+                            name={exchange.name}
+                            imageSrc={exchange.imageSrc}
+                            exchangeWith={exchange.exchangeWith}
+                            notes={exchange.notes}
+                            contact={exchange.contact}
+                            date={exchange.date}
+                            isFavorite={exchange.isFavorite}
+                        />
+                    ))}
+                </div>
+            </ContentBox>
+        </ProtectedRoute>
+    );
+}
---
+++ src/app/(profile)/posts/exchange/page.jsx
@@ -0,0 +1,27 @@
+"use client";
+import ProtectedRoute from '@/components/ProtectedRoute';
+import ContentBox from '@/components/UI/profile/ContentBox';
+import ExchangeCard from '../../../../components/profile/ExchangeCard'
+
+export default function ProfilePage() {
+    return (
+        <ProtectedRoute>
+            <ContentBox title="Exchange">
+                <div className="flex flex-col justify-center">
+                    <ExchangeCard
+                        publisher="Dr Ahmed"
+                        name="Cannies"
+                        imageSrc="/images/Profile/mirror.svg"
+                        exchangeWith="Second molar"
+                        notes="The tooth has a cavity."
+                        contact="015080568"
+                        date="21/1/2025"
+                        isFavorite={false}
+                        onEdit={(updatedData) => console.log("Updated Patient:", updatedData)}
+
+                    />
+                </div>
+            </ContentBox>
+        </ProtectedRoute>
+    );
+}
