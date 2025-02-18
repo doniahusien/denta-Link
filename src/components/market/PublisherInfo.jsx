bb16e7b9c9c927a1c76ed1252e2bab540f6a408a
doniahusien <doniahusien@gmail.com>
2025-02-18 22:36:04 +0200

    fix: update PublisherInfo component for new marketplace structure

--- src/components/market/PublisherInfo.jsx
+++ src/components/market/PublisherInfo.jsx
@@ -1,13 +1,15 @@
 'use client';
 import React from 'react';
-
+import Link from 'next/link';
 const PublisherInfo = ({ publisher, address, contact }) => {
     return (
         <div className="shadow-md p-10 lg:w-2/3">
             <div className="space-y-4">
                 <p className="text-lg text-gray-500">About publisher:</p>
                 <div>
-                    <span className="font-semibold">{publisher.name}: </span>
+                    <Link href="/market/shop/specificShop">
+                        <span className="font-semibold">{publisher.name}: </span>
+                    </Link>
                     <span className="text-gray-600 text-sm">{publisher.description}</span>
                 </div>
                 <p className="text-sm">
