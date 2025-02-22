3a28d8f8ea50302e49871d4d8dc5ed95d456488e
doniahusien <doniahusien@gmail.com>
2025-02-22 13:17:49 +0200

    Improve exchange posts page responsiveness

--- src/app/(profile)/posts/exchange/page.jsx
+++ src/app/(profile)/posts/exchange/page.jsx
@@ -1,26 +1,37 @@
 "use client";
+import { useEffect } from "react";
 import ProtectedRoute from "@/components/ProtectedRoute";
 import ContentBox from "@/components/UI/profile/ContentBox";
 import ExchangeCard from "../../../../components/profile/ExchangeCard";
-
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchMyExchanges } from "@/redux/features/profile/profileThunk";
 export default function ProfilePage() {
+  const dispatch = useDispatch();
+  const { myExchanges, loading } = useSelector(state => state.profile);
+  useEffect(() => {
+    dispatch(fetchMyExchanges());
+  }, [dispatch]);
   return (
     <ProtectedRoute>
       <ContentBox title="Exchange">
         <div className="flex flex-col justify-center">
-          <ExchangeCard
-            publisher="Dr Ahmed"
-            name="Cannies"
-            imageSrc="/images/Profile/mirror.svg"
-            exchangeWith="Second molar"
-            notes="The tooth has a cavity."
-            contact="015080568"
-            date="21/1/2025"
-            isFavorite={false}
-            onEdit={(updatedData) =>
-              console.log("Updated Patient:", updatedData)
-            }
-          />
+          {myExchanges.map((tool, index) => (
+            <ExchangeCard
+              key={index}
+              name={tool.name}
+              publisher={tool.publisher.name}
+              toothName={tool.toothName}
+              imageSrc={tool.images[0]}
+              exchangeWith={tool.exchangeWith}
+              notes={tool.notes}
+              contact={tool.contact}
+              createdAt={tool.createdAt}
+              isFavorite={false}
+              exchangeId={tool._id}
+              onEdit={(updatedData) => handleEditExchange(tool._id, updatedData)}
+            />
+          ))}
+
         </div>
       </ContentBox>
     </ProtectedRoute>
