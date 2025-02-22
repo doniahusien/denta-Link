cb17d31adb1ed3bfdbdd9c8df85b901625e10cf2
doniahusien <doniahusien@gmail.com>
2025-02-22 13:17:20 +0200

    Modify profileThunk to handle patient and exchange updates

--- src/redux/features/profile/profileThunk.jsx
+++ src/redux/features/profile/profileThunk.jsx
@@ -56,7 +56,6 @@ export const fetchPatientFav = createAsyncThunk(
     }
 );
 
-
 export const updatePatient = createAsyncThunk(
     "profile/updatePatient",
     async ({ patientId, formData }, { dispatch, rejectWithValue, getState }) => {
@@ -87,6 +86,7 @@ export const updatePatient = createAsyncThunk(
     }
 );
 
+//exchange 
 export const fetchExchangeFav = createAsyncThunk(
     "profile/fetchExchangeFav",
     async (_, { rejectWithValue, getState }) => {
@@ -103,12 +103,70 @@ export const fetchExchangeFav = createAsyncThunk(
             if (!response.ok) {
                 const errorData = await response.json();
                 console.error("API Error:", errorData);
-                throw new Error(errorData.message || "Failed to fetch patient favourits");
+                throw new Error(errorData.message || "Failed to fetch exchanges tool favourits");
             }
             const data = await response.json();
             return data;
         } catch (error) {
             console.error("Fetch Error:", error.message);
+            return rejectWithValue(error.message);
+        }
+    }
+);
+
+export const fetchMyExchanges = createAsyncThunk(
+    "profile/fetchMyExchanges",
+    async (_, { rejectWithValue, getState }) => {
+        try {
+            const state = getState();
+            const token = state.auth.token;
+
+            const response = await fetch("http://localhost:3000/api/exchanges/my-exchanges", {
+                method: "GET",
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                }
+            });
+
+            if (!response.ok) {
+                const errorData = await response.json();
+                console.error("API Error:", errorData);
+                throw new Error(errorData.message || "Failed to fetch exchange tools");
+            }
+
+            const data = await response.json();
+            return data;
+        } catch (error) {
+            console.error("Fetch Error:", error.message);
+            return rejectWithValue(error.message);
+        }
+    }
+);
+
+
+export const updateExchange = createAsyncThunk(
+    "profile/updateExchange",
+    async ({ exchangeId, formData }, { dispatch, rejectWithValue, getState }) => {
+        try {
+            const state = getState();
+            const token = state.auth.token;
+            const response = await fetch(`http://localhost:3000/api/exchanges/update/${exchangeId}`, {
+                method: "PUT",
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(formData),
+            });
+            if (!response.ok) {
+                const errorData = await response.json();
+                console.error("Update Error:", errorData);
+                return rejectWithValue(errorData.message || "Failed to update exchange.");
+            }
+            const data = await response.json();
+            dispatch(fetchMyExchanges()); 
+            return data;
+        } catch (error) {
             return rejectWithValue(error.message);
         }
     }
