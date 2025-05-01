467a2c17295eb5d8868cc13d74428464918e466a
doniahusien <doniahusien@gmail.com>
2025-05-01 15:34:02 +0300

    refactor(tool): improve tool listing performance

--- src/components/market/ToolList.jsx
+++ src/components/market/ToolList.jsx
@@ -1,98 +1,32 @@
+"use client";
 import React from 'react';
 import ToolCard from './ToolCard';
-
+import { useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
+import { fetchAllTools } from '@/redux/features/tools/toolThunk';
+import { useEffect } from 'react';
 const ToolList = () => {
-    const products = [{
-        id: 1,
-        image: "/images/market/tool.svg",
-        name: "Curette",
-        price: 200,
-        description: "for deep cleaning...",
-    },
-    {
-        id: 2,
-        image: "/images/market/tool.svg",
-        name: "Scaler",
-        price: 150,
-        description: "for plaque removal...",
-    },
-    {
-        id: 3,
-        image: "/images/market/tool.svg",
-        name: "Explorer",
-        price: 100,
-        description: "for cavity detection...",
-    },
-    {
-        id: 89,
-        image: "/images/market/tool.svg",
-        name: "Explorer",
-        price: 100,
-        description: "for cavity detection...",
-    },
-    {
-        id: 60,
-        image: "/images/market/tool.svg",
-        name: "Explorer",
-        price: 100,
-        description: "for cavity detection...",
-    },
-    {
-        id: 70,
-        image: "/images/market/tool.svg",
-        name: "Explorer",
-        price: 100,
-        description: "for cavity detection...",
-    },
-    {
-        id: 55,
-        image: "/images/market/tool.svg",
-        name: "Explorer",
-        price: 100,
-        description: "for cavity detection...",
-    },
-    {
-        id: 40,
-        image: "/images/market/tool.svg",
-        name: "Explorer",
-        price: 100,
-        description: "for cavity detection...",
-    },
-    {
-        id: 30,
-        image: "/images/market/tool.svg",
-        name: "Explorer",
-        price: 100,
-        description: "for cavity detection...",
-    },
-    {
-        id: 10,
-        image: "/images/market/tool.svg",
-        name: "Explorer",
-        price: 100,
-        description: "for cavity detection...",
-    },
-    {
-        id: 11,
-        image: "/images/market/tool.svg",
-        name: "Explorer",
-        price: 100,
-        description: "for cavity detection...",
-    },
-    ];
+    const { loading, tools, searchTool } = useSelector(state => state.tool);
+    const dispatch = useDispatch();
+    useEffect(() => {
+        if (!searchTool) {
+            dispatch(fetchAllTools());
+        }
+    }, [dispatch, searchTool]);
+    if (loading) return (<p>loading</p>)
 
     return (
         <div className="min-h-screen px-5 py-10 ">
             <h1 className="text-4xl my-12 sm:pl-10 md:pl-20">Tools</h1>
             <div className="flex flex-row flex-wrap justify-center gap-4 sm:px-20 md:px-0">
-                {products.map((product, index) => (
+                {tools.map((tool, index) => (
                     <ToolCard
                         key={index}
-                        image={product.image}
-                        name={product.name}
-                        id={product.id}
-                        price={product.price}
-                        description={product.description}
+                        image={tool.images[0]}
+                        name={tool.toolName}
+                        id={tool._id}
+                        price={tool.price}
+                        description={tool.description}
                     />
                 ))}
             </div>
