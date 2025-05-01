32b4522987eb1021fdb13c4d78ee354773186328
doniahusien <doniahusien@gmail.com>
2025-05-01 15:34:03 +0300

    chore(redux): add new features to store setup

--- src/redux/store.jsx
+++ src/redux/store.jsx
@@ -3,13 +3,16 @@ import authReducer from './features/auth/authSlice';
 import patientReducer from './features/patient/patientSlice';
 import profileReducer from './features/profile/profileSlice'
 import exchangeReducer from './features/exchange/exchangeSlice';
+import cartReducer from './features/cart/cartSlice';
+import toolReducer from './features/tools/toolSlice';
 const store = configureStore({
     reducer: {
         auth: authReducer,
         patient: patientReducer,
         profile: profileReducer,
         exchange:exchangeReducer,
-        
+        tool:toolReducer,
+        cart:cartReducer
     }
 });
 export default store
