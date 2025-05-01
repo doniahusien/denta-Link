1f3d28c91a51a0c3c0c769f879c9fc5b6be23112
doniahusien <doniahusien@gmail.com>
2025-05-01 15:33:55 +0300

    style(ui): set fixed width and height for detail images

--- src/components/UI/DetailImage.jsx
+++ src/components/UI/DetailImage.jsx
@@ -3,12 +3,13 @@ import React from 'react';
 import Image from 'next/image';
 import Fav from './Fav';
 
-const DetailImage = ({ image, alt,fav,patientId }) => {
+const DetailImage = ({ image, alt,fav,patientId, tool}) => {
 
     return (
-        <div className="relative mb-8">
-            <Image src={image[0]} alt={alt} width={100} height={200} className="w-full h-64 object-cover" />
-            <Fav fav={fav} patientId={patientId} />
+        <div className="relative mb-8 h-full">
+            <Image src={image[0]} alt={alt} width={200} height={500} className="w-full h-full object-cover" />
+            {!tool&&<Fav fav={fav} patientId={patientId} />}
+            
         </div>
     );
 };
