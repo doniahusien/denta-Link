a23e58951d00f8205dc6362e439afa07c6f4e428
doniahusien <doniahusien@gmail.com>
2025-05-01 15:33:58 +0300

    fix(cart): update cart item layout and quantity logic

--- src/components/cart/cartItem.jsx
+++ src/components/cart/cartItem.jsx
@@ -1,26 +1,32 @@
 import Image from 'next/image';
 import { Trash2, Plus, Minus } from 'lucide-react';
-
-const CartItem = ({ item, updateQuantity, removeFromCart }) => {
+import { useDispatch } from 'react-redux';
+import { decrementCartItem, removeCartItem } from '@/redux/features/cart/cartThunk';
+import { incrementCartItem } from '@/redux/features/cart/cartThunk';
+const CartItem = ({ item }) => {
+  const dispatch = useDispatch();
   return (
     <div className="flex flex-col md:flex-row items-center p-4 border rounded-lg shadow-sm space-y-4 md:space-y-0">
       <div className="w-24 h-24 flex-shrink-0">
-        <Image src={`/images/Cart/${item.image}`} alt={item.name} width={80} height={80} />
+        <Image src={item.tool.images[0]} alt={item.tool.toolName} width={80} height={80} />
       </div>
       <div className="flex-1 text-center md:text-left">
-        <h3 className="text-lg font-semibold">{item.name}</h3>
-        <p className="text-gray-600">{item.description}</p>
-        <p className="text-blue-600 font-semibold">{item.price} LE</p>
+        <h3 className="text-lg font-semibold">{item.tool.toolName}</h3>
+        <p className="text-gray-600">{item.tool.description}</p>
+        <p className="text-blue-600 font-semibold">{item.itemTotalPrice} LE</p>
       </div>
       <div className="flex items-center space-x-2">
-        <button className="p-1 border rounded" onClick={() => updateQuantity(item.id, item.quantity - 1)}>
+        <button className="p-1 border rounded" onClick={() =>
+          dispatch(decrementCartItem(item.tool._id))}>
           <Minus size={16} />
         </button>
         <span className="px-2">{item.quantity}</span>
-        <button className="p-1 border rounded" onClick={() => updateQuantity(item.id, item.quantity + 1)}>
+        <button className="p-1 border rounded" onClick={() =>
+          dispatch(incrementCartItem(item.tool._id))}>
           <Plus size={16} />
         </button>
-        <button className="ml-2 text-red-600" onClick={() => removeFromCart(item.id)}>
+        <button className="ml-2 text-red-600" onClick={() =>
+          dispatch(removeCartItem(item.tool._id))}>
           <Trash2 size={18} />
         </button>
       </div>
