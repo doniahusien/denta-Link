11d2eec6b599e111790dfba83f7b68bf0115c2ad
doniahusien <doniahusien@gmail.com>
2025-05-01 15:33:59 +0300

    refactor(exchange): restructure tool list display

--- src/components/market/ExchangeToolList.jsx
+++ src/components/market/ExchangeToolList.jsx
@@ -14,9 +14,9 @@ const ExchangeToolList = () => {
     }, [dispatch, searchTool]);
     if (loading) return (<p>loading</p>)
     return (
-        <div className="min-h-screen px-5 py-10">
-            <h1 className="text-4xl my-12 sm:pl-10 md:pl-20">Tools</h1>
-            <div className="flex flex-wrap justify-center gap-6">
+        <div className="min-h-screen px-8 py-10">
+            <h1 className="text-4xl my-12 sm:pl-8 md:pl-16">Tools</h1>
+            <div className="flex flex-wrap justify-center items-start gap-5">
                 {exchangeTools.map((tool) => (
                     <ExchangeCard key={tool._id} id={tool._id} imageSrc={tool.images[0]} {...tool} />
                 ))}
