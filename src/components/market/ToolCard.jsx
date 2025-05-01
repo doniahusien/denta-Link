a0d0839bc91bd8f823e0aaf08d5767eb1958726e
doniahusien <doniahusien@gmail.com>
2025-05-01 15:34:01 +0300

    style(tool): fix image size and align content

--- src/components/market/ToolCard.jsx
+++ src/components/market/ToolCard.jsx
@@ -2,7 +2,7 @@
 import React from 'react'
 import { ChevronRight } from 'lucide-react'
 import { useRouter } from 'next/navigation'
-const ToolCard = ({ id,image, name, price, description }) => {
+const ToolCard = ({ id, image, name, price, description }) => {
     const router = useRouter();
     const handleButtonClick = (e) => {
         e.stopPropagation();
@@ -10,7 +10,7 @@ const ToolCard = ({ id,image, name, price, description }) => {
     }
     return (
         <div className=" bg-white border border-gray-200 rounded-lg  transition-transform duration-300 ease-in-out hover:scale-105 hover:brightness-125 hover:border-blue-500 shadow-md px-4 py-3 w-full sm:w-[40%] md:w-[35%] lg:w-[17%]">
-            <img src={image} alt={name} className="w-full h-55 object-cover rounded-t-lg" />
+            <img src={image} alt={name} className="w-[150px] h-[150px] object-cover rounded-t-lg" />
             <div className="mt-4">
                 <h2 className="text-lg font-semibold">{name}</h2>
                 <p className="text-gray-500">Price: <span className="text-black">{price}</span></p>
