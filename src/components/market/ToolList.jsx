f89880ccea1cabe12a1d58f59feaba3f8358f6b0
doniahusien <doniahusien@gmail.com>
2025-05-31 03:50:50 +0300

    feat(market): update tool list component

--- src/components/market/ToolList.jsx
+++ src/components/market/ToolList.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import ToolCard from './ToolCard';
 import { useSelector } from 'react-redux';
 import { useDispatch } from 'react-redux';
+import Loader from '../UI/Loader/Loader';
 import { fetchAllTools } from '@/redux/features/tools/toolThunk';
 import { useEffect } from 'react';
 const ToolList = () => {
@@ -13,25 +14,30 @@ const ToolList = () => {
             dispatch(fetchAllTools());
         }
     }, [dispatch, searchTool]);
-    if (loading) return (<p>loading</p>)
-
-    return (
-        <div className="min-h-screen px-5 py-10 ">
-            <h1 className="text-4xl my-12 sm:pl-10 md:pl-20">Tools</h1>
-            <div className="flex flex-row flex-wrap justify-center gap-4 sm:px-20 md:px-0">
-                {tools.map((tool, index) => (
-                    <ToolCard
-                        key={index}
-                        image={tool.images}
-                        name={tool.toolName}
-                        id={tool._id}
-                        price={tool.price}
-                        description={tool.description}
-                    />
-                ))}
-            </div>
+    if (loading) return (
+        <div className="min-h-screen flex justify-center items-center">
+            <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-blue-500"></div>
         </div>
-    );
+    )
+
+
+return (
+    <div className="min-h-screen px-5 py-10 ">
+        <h1 className="text-4xl my-12 sm:pl-10 md:pl-20">Tools</h1>
+        <div className="flex flex-row flex-wrap justify-center gap-4 sm:px-20 md:px-0">
+            {tools.map((tool, index) => (
+                <ToolCard
+                    key={index}
+                    image={tool.images}
+                    name={tool.toolName}
+                    id={tool._id}
+                    price={tool.price}
+                    description={tool.description}
+                />
+            ))}
+        </div>
+    </div>
+);
 };
 
 export default ToolList;
